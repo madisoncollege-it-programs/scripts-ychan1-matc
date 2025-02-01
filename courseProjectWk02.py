bf49585f19b417b7ada5c5ff7f73ec3399dbdfec
ychan1-matc <ychan1@madisoncollege.edu>
2025-02-01 09:43:55 -0600

    Submission Milestone 02 fir Grading.

---
+++ courseProjectWk02.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Author: Crystal Chan
+Email: ychan1@madisoncollege.edu
+Description: <Semester-long course project script which will analyze 
+an Apache web log to determine current threats>
+"""
+
+strUserInput = input("What is your name?\n>>>") 
+print(f"Welcome, {strUserInput}!") 
