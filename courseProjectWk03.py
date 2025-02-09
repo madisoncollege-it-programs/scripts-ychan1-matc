f2c6c9637e03f30b39bdaa582d5e4c56f7c985c6
ychan1-matc <ychan1@madisoncollege.edu>
2025-02-09 10:44:58 -0600

    Submitting Milestone 03 for Grading.

---
+++ courseProjectWk03.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Author: Crystal Chan
+Email: ychan1@madisoncollege.edu
+Description: <Semester-long course project script which will analyze 
+an Apache web log to determine current threats>
+"""
+
+strUserInput = input("What is your name?\n>>>") 
+print(f"Welcome, {strUserInput}!") 
+
+strLogLine = '111.222.333.123 HOME - [01/Feb/1998:01:08:39 -0800] "GET /bannerad/ad.htm HTTP/1.0" 200 198 "http://www.referrer.com/bannerad/ba_intro.htm" "Mozilla/4.01 (Macintosh; I; PPC)"'
+
+listLogLine = strLogLine.split(' ')
+print(f"Log request from  ***{listLogLine[0]}****")
+print(f"Return Code:  {listLogLine[8]}")
