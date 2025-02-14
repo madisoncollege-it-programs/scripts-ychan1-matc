f2709e0886f21115241f175492eff5e1ceb597ae
ychan1-matc <ychan1@madisoncollege.edu>
2025-02-13 23:46:09 -0600

    Resubmission 2.0. Changed line 15

--- courseProjectWk03.py
+++ courseProjectWk03.py
@@ -11,7 +11,6 @@ print(f"Welcome, {strUserInput}!")
 
 strLogLine = '111.222.333.123 HOME - [01/Feb/1998:01:08:39 -0800] "GET /bannerad/ad.htm HTTP/1.0" 200 198 "http://www.referrer.com/bannerad/ba_intro.htm" "Mozilla/4.01 (Macintosh; I; PPC)"'
 
-
-print(f"Log request from: {strLogLine[:15]:*^22}")
 listLogLine = strLogLine.split(" ")
+print(f"Log request from: {listLogLine[0]:*^22}")
 print(f"Return Code: {listLogLine[8]}")
