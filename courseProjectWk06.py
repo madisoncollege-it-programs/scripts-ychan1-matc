b405e703b730f273a488afa19014edda7ae3fe34
ychan1-matc <ychan1@madisoncollege.edu>
2025-03-02 13:44:59 -0600

    Submitting Milestone 06 for Grading.

---
+++ courseProjectWk06.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+Author: Crystal Chan
+Email: ychan1@madisoncollege.edu
+Description: <Semester-long course project script which will analyze 
+an Apache web log to determine current threats>
+"""
+import sys
+
+
+if len(sys.argv) == 1:
+    strUserInput = input("Would you like to continue? (y/n)\n>>>") 
+    if strUserInput.lower() in ['y', 'yes', 'yep', 'yup', 'yeah']:
+        varSwitch = True
+    else:
+        varSwitch = False
+
+elif f"{sys.argv[1]}".lower() in ['y', 'yes', 'yep', 'yup', 'yeah']:
+    varSwitch = True
+else:
+    varSwitch = False
+
+
+if varSwitch == True:
+    with open("06_CP-Access.log") as hFile:
+        strLogLines = hFile.read()
+    listLogLines = strLogLines.split('\n')
+    with open("courseProjectWk06Anlysis.txt", "w") as wrapperLogFile:
+        for strLogLine in listLogLines:
+            listLogLine = strLogLine.split(" ")
+            strReturnCode = f"{listLogLine[8]}" 
+            strIPReturnCode = f"{listLogLine[0]}" + " - " + strReturnCode
+            if strReturnCode >= "400":
+                print(strIPReturnCode)
+            if strReturnCode >= "500":
+                wrapperLogFile.write(f"{strIPReturnCode}\n")
+
