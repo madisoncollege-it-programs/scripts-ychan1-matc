2d3917350f484499d03cf36b3507888c85d2854f
ychan1-matc <ychan1@madisoncollege.edu>
2025-03-07 15:18:10 -0600

    Resubmission of Milestone 06 for Grading

--- courseProjectWk06.py
+++ courseProjectWk06.py
@@ -13,7 +13,7 @@ if len(sys.argv) > 1:
 else:
     strUserInput = input("Would you like to continue? (y/n)\n>>>") 
 
-if strUserInput.lower() in ['y', 'yes', 'yep', 'yup', 'yeah']:s
+if strUserInput.lower() in ['y', 'yes', 'yep', 'yup', 'yeah']:
     with open("06_CP-Access.log") as hFile:
         strLogLines = hFile.read()
     listLogLines = strLogLines.split('\n')
@@ -21,7 +21,7 @@ if strUserInput.lower() in ['y', 'yes', 'yep', 'yup', 'yeah']:s
         for strLogLine in listLogLines:
             listLogLine = strLogLine.split(" ")
             strReturnCode = f"{listLogLine[8]}" 
-            strIPReturnCode = f"{listLogLine[0]}" + " - " + strReturnCode
+            strIPReturnCode = f"{listLogLine[0]} - {strReturnCode}"
             if strReturnCode >= "400":
                 print(strIPReturnCode)
             if strReturnCode >= "500":
