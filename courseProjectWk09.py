697fe5ea7133a0e0411c4f4a6a7ef094ab619a04
ychan1-matc <ychan1@madisoncollege.edu>
2025-03-30 16:33:24 -0500

    Submitting milestone 9 for grading.

---
+++ courseProjectWk09.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""
+Author: Crystal Chan
+Email: ychan1@madisoncollege.edu
+Description: <Semester-long course project script which will analyze 
+an Apache web log to determine current threats>
+"""
+import sys
+
+def parseLogEntry(strLogLine):
+    listLogLine = strLogLine.split(" ")
+    strIP = f"{listLogLine[0]}"
+    strReturnCode = f"{listLogLine[8]}" 
+    return [strIP,strReturnCode]
+    
+
+
+
+def main():
+    
+    if len(sys.argv) > 1:
+        strUserInput =f"{sys.argv[1]}"
+    else:
+        strUserInput = input("Would you like to continue? (y/n)\n>>>") 
+
+    if strUserInput.lower() in ['y', 'yes', 'yep', 'yup', 'yeah']:
+        with open("06_CP-Access.log") as hFile:
+            strLogLines = hFile.read()
+        listLogLines = strLogLines.split('\n')
+        with open("courseProjectWk06Anlysis.txt", "w") as wrapperLogFile:
+            dictLogSummary = {}
+            for strLogLine in listLogLines:
+                listReturn = parseLogEntry(strLogLine)
+                strIP = listReturn[0]
+                strReturnCode = listReturn[1]
+                if strReturnCode >= "400":
+                    print(f"{strIP} - {strReturnCode}")
+                if strIP in dictLogSummary:
+                    intCounter = dictLogSummary[strIP]
+                    intCounter += 1
+                    dictLogSummary[strIP] = intCounter
+                else:
+                    dictLogSummary[strIP] = 1
+            with open("courseProjectWk09Analysis.csv", "w") as wrapperLogFile:
+                wrapperLogFile.write(f"IP Address,Hits\n")
+                for strIP, intCounter in dictLogSummary.items():
+                    if intCounter >= 5:
+                        wrapperLogFile.write(f"{strIP},{intCounter}\n")
+
+
+if __name__ == "__main__":
+    main()
+
+
+    
