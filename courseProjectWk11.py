cbb2bb76c1a637edc44535585e16f3e29dd8bd01
ychan1-matc <ychan1@madisoncollege.edu>
2025-04-12 15:04:15 -0500

    Submitting Milestone 11 for Grading.

---
+++ courseProjectWk11.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+Author: Crystal Chan
+Email: ychan1@madisoncollege.edu
+Description: <Semester-long course project script which will analyze 
+an Apache web log to determine current threats>
+"""
+import argparse
+import subprocess
+
+
+
+def ipAddressCount(inApacheLogFileName):
+    command = f"cat {inApacheLogFileName} | cut -d ' ' -f1 | sort -n | uniq -c | sort -n | tail -n5"
+    process = subprocess.run(command, shell=True, stdout=subprocess.PIPE)
+    output = process.stdout.decode()
+    return output
+
+
+
+def main(): 
+    
+    parser = argparse.ArgumentParser(description = "Analyze Apache log file for top IP address")
+    parser.add_argument('-i','--infilename', type=str, required=True, help='Path to Apahe log file')
+    parser.add_argument('-o','--outfilename', type=str, help='Output file to save result')
+
+    args = parser.parse_args()
+
+    strLogLines = ipAddressCount(args.infilename)
+
+    if args.outfilename:
+        with open(args.outfilename, "w") as wrapperLogFile:
+            wrapperLogFile.write(strLogLines)
+    else:
+        print(strLogLines)
+
+if __name__ == "__main__":
+    main()
